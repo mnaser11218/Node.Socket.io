af0bdb15491943f4b7ab301151a5424b3049ac09
Mohammed N <mnaser11218@gmail.com>
2025-04-13 18:34:52 -0400

    added backend connection for justWs servers js page

--- practiceSocketFiles/justWSserver.js
+++ practiceSocketFiles/justWSserver.js
@@ -4,6 +4,15 @@ const websocket = require('ws')
 const server = http.createServer((req,res)=>{
     res.end('I am connected!')
 })
-
 const wss = new websocket.WebSocketServer({server})
+
+wss.on('headers', (headers, req)=>{
+    console.log(headers)
+})
+
+wss.on('connection', (ws, req)=>{
+    ws.send('Welcome to the websocket server!!')
+})
+
+
 server.listen(8000)
