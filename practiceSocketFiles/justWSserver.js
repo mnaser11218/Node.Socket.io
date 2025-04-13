c70340a18fa470453ee6d5002ac171b6cc619132
Mohammed N <mnaser11218@gmail.com>
2025-04-13 19:15:15 -0400

    added comments to browser and server pages for just ws

--- practiceSocketFiles/justWSserver.js
+++ practiceSocketFiles/justWSserver.js
@@ -1,17 +1,24 @@
+// server backend
+
 const http = require('http')
 const websocket = require('ws')
 
+// creating http server 
 const server = http.createServer((req,res)=>{
     res.end('I am connected!')
 })
+
+// creating websocket server and listening to http server
 const wss = new websocket.WebSocketServer({server})
 
-wss.on('headers', (headers, req)=>{
-    console.log(headers)
-})
 
+
+// once we connect, server will send message to browser and listen for message sent back. 
 wss.on('connection', (ws, req)=>{
     ws.send('Welcome to the websocket server!!')
+    ws.on('message', data=>{
+        console.log(data.toString())
+    })
 })
 
 
