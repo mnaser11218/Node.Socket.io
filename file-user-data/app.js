4a17605bec364d28d17681faf17d9b698ba37623
Mohammed N <mnaser11218@gmail.com>
2025-04-16 17:39:49 -0400

    added request method for post request

--- file-user-data/app.js
+++ file-user-data/app.js
@@ -10,10 +10,24 @@
 // })
 const http = require('http')
 const server = http.createServer((req, res) => {
-    console.log("Incoming request")
-    console.log(req.method, req.url)
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<form method="POST"><input type="text" name="username"><button>Create User</button></input></form>')
+
+    //request if http req is a post request, get data from request body and send it as a response 
+    if(req.method === "POST"){
+        let body = ""
+        req.on('end', ()=>{
+         res.end(`<h1>The name is ${body.split("=")[1]}</h1>`)
+        })
+        req.on('data', (data)=>{
+            body += data
+        })
+        
+    }else{
+        // if http request is not get request, send a form as a response
+        res.setHeader('Content-Type', 'text/html')
+        res.end('<form method="POST"><input type="text" name="username"><button>Create User</button></input></form>')
+    }
+
 })
 
+// listen to http request made to localhost:5000
 server.listen(5000)
--- socketIO101/basics.js
+++ socketIO101/basics.js
@@ -2,18 +2,18 @@
 const express = require('express');
 const app = express();
 const socketio = require('socket.io')
-console.log("inside")
+
 
 app.use(express.static(__dirname + '/public'));
 
 const expressServer = app.listen(8000);
 const io = socketio(expressServer)
 
-io.on('connection',(socket)=>{
-    console.log(socket.id,"has connected")
+io.on('connection', (socket) => {
+    console.log(socket.id, "has connected")
     //in ws we use "send" method, and it socket.io we use the "emit" method
-    socket.emit('messageFromServer',{data:"Welcome to the socket server!"})
-    socket.on('messageFromClient',(data)=>{
-        console.log("Data:",data);
+    socket.emit('messageFromServer', { data: "Welcome to the socket server!" })
+    socket.on('messageFromClient', (data) => {
+        console.log("Data:", data);
     })
 })
